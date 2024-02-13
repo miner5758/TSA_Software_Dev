63681d8193303ead49a3c2c6d635402bc149c5ce
Paul Afriyie <paul.afriyie19@gmail.com>
2024-02-12 17:08:26 -0700

    added login screen and worked on home screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,43 +1,116 @@
-import 'package:flutter/material.dart'; 
-import 'dart:async'; 
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:app_usage/app_usage.dart';
+
+Future<Duration> _getUsageStats() async {
+  List<AppUsageInfo> _infos = [];
+  Duration combined = Duration();
+  try {
+    DateTime endDate = DateTime.now();
+    DateTime startDate = endDate.subtract(Duration(hours: 24));
+    List<AppUsageInfo> infoList =
+        await AppUsage().getAppUsage(startDate, endDate);
+    _infos = infoList;
+
+    for (var info in infoList) {
+      combined += info.usage;
+      //print(info.usage.inHours);
+    }
+  } on AppUsageException catch (exception) {
+    print(exception);
+  }
+  return combined;
+}
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
- 
-  @override 
-  _MyHomePageState createState() => _MyHomePageState(); 
-} 
-class _MyHomePageState extends State<MyHomePage> { 
-  @override 
-  void initState() { 
-    super.initState(); 
-    Timer(const Duration(seconds: 3), 
-          ()=>Navigator.pushReplacement(context, 
-                                        MaterialPageRoute(builder: 
-                                                          (context) =>  
-                                                          SecondScreen() 
-                                                         ) 
-                                       ) 
-         ); 
-  } 
-  @override 
-  Widget build(BuildContext context) { 
-    return Container( 
-      color: Colors.white, 
-      child:FlutterLogo(size:MediaQuery.of(context).size.height) 
-    ); 
-  } 
-} 
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => SecondScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child: FlutterLogo(size: MediaQuery.of(context).size.height));
+  }
+}
+
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
- 
-  @override 
-  Widget build(BuildContext context) { 
-    return Scaffold( 
-      appBar: AppBar(title:const Text("GeeksForGeeks")), 
-      body: const Center( 
-        child:Text("Home page",) 
-      ), 
-    ); 
-  } 
-} 
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+                      future: _getUsageStats(),
+                      builder: (context, AsyncSnapshot lnapshots) {
+                        if (lnapshots.hasData) {
+                  
+                            return Scaffold(
+              body: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color(0xff4169e1),
+                        Color(0xffFFE5B4),
+                      ],
+                      stops: [0, 0],
+                    ),
+                  ),
+                  child: ListView.builder(
+                      padding: const EdgeInsets.all(1.0),
+                      itemCount: 1,
+                      itemBuilder: (context, index) {
+                        return Column(children: [    
+                          SizedBox(
+                              height: MediaQuery.of(context).size.height * .10),                  
+                          Container(
+                              alignment: Alignment.center,
+                              height: MediaQuery.of(context).size.height * .20,
+                              child: Card(
+                                  child: InkWell(
+                                splashColor: Colors.blue,
+                                onTap: () {},
+                                child: Row(children: <Widget>[
+                                  Expanded(
+                                    flex: 5,
+                                    child: Padding(
+                                      padding: EdgeInsets.only(left: 19.0),
+                                      child: Center(child:Text(
+                                        "Total Screen time: ${lnapshots.data.inHours}",
+                                        style: const TextStyle(
+                                            fontSize: 11,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      )
+                                    ),
+                                  ),
+                                  
+                                ]),
+                              )))
+                        ]);
+                      })));
+                        } else if (lnapshots.hasError) {
+                          return Text("${lnapshots.error}");
+                        }
+                        // By default show a loading spinner.
+                        return const CircularProgressIndicator();
+                      });
+                
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,58 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+import 'home.dart';
+import 'authentication_service.dart';
+import 'login.dart';
 
-import 'home.dart'; 
-void main() { 
-  runApp(MyApp()); 
-} 
-  
-class MyApp extends StatelessWidget { 
-  @override 
-  Widget build(BuildContext context) { 
-    return MaterialApp( 
-      title: 'Splash Screen', 
-      theme: ThemeData( 
-        primarySwatch: Colors.green, 
-      ), 
-      home: const MyHomePage(), 
-      debugShowCheckedModeBanner: false, 
-    ); 
-  } 
-} 
-  
+final FirebaseAuth auth = FirebaseAuth.instance;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  static const String _title = 'Zenex';
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider(create: (_) => AuthenticationService(FirebaseAuth.instance)),
+        StreamProvider(
+            create: (context) =>
+                context.read()<AuthenticationService>().authStateChanges,
+            initialData: null)
+      ],
+      child: const MaterialApp(
+        title: _title,
+        home: AuthenticationWrapper(),
+      ),
+    );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  const AuthenticationWrapper({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const MyHomePage();
+            } else {
+              return const Logscreen();
+            }
+          }),
+    );
+  }
+}
